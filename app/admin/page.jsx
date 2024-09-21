c063dcb8ec13b67f6bd29c645c126ba97fb37709
Niffyjade <adefisayooluwanifemi@yahoo.com>
2024-09-21 10:45:15 +0100

    new commit

---
+++ app/admin/page.jsx
@@ -0,0 +1,12 @@
+import Sidebar from "@/Components/AdminComponents/Sidebar";
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default page
